7980dff08a0ee8db6d86940e4cbb6d184aa98e6d
Andrey Fedyushev <rdshatv@gmail.com>
2023-11-15 02:45:29 +0700

    upd1

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const { PORT = 3000, BASE_PATH = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 4000, BASE_PATH = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 module.exports = {
   PORT,
